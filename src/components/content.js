4ca8e982aa090df933698e5048e3188b755751ab
Philipp Bauch <ga53xex@mytum.de>
2019-11-23 13:04:38 +0100

    Add bottom bar

--- src/components/content.js
+++ src/components/content.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Canvas from "./canvas";
 import './content.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBroadcastTower } from '@fortawesome/free-solid-svg-icons';
 
 const Content = ({ children }) => {
   return (
     <div className="app-content">
+        <div className="bottom-bar">
+          <input spellCheck={false} className="message-input" type="text"/>
+          <div className="send-button">
+            <FontAwesomeIcon icon={faBroadcastTower} />
+          </div>
+        </div>
         <Canvas />
     </div>
   );
